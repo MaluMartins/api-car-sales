e536bd1e2b077fb8024a9c2158da59078f73f5ed
Malu Martins <martinss.mmalu@gmail.com>
2025-06-30 21:41:04 -0300

    feat: criar controllers de post e owner

---
+++ src/main/java/com/portal/apiCarros/controller/CarPostController.java
@@ -0,0 +1,50 @@
+package com.portal.apiCarros.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.portal.apiCarros.dto.CarPostDTO;
+import com.portal.apiCarros.service.CarPostStoreService;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarPostController {
+	
+	@Autowired
+	private CarPostStoreService carPostStoreService; 
+	
+	@GetMapping("/posts")
+	public ResponseEntity<List<CarPostDTO>> getCarSales() {
+		return ResponseEntity.status(HttpStatus.FOUND).body(carPostStoreService.getCarsForSale());
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id) {
+		carPostStoreService.changeCarForSale(carPostDTO, id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity deleteCarForSale(@PathVariable("id") String id) {
+		carPostStoreService.removeCarForSale(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+}
+
+
+
+
+
+
+
